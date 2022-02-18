c5dda76fef341c12579a38bacca073a8f633a956
Dhairya Upadhyay <94118693+Sharlock221b-svg@users.noreply.github.com>
2022-02-18 10:36:27 +0000

    All FCC test Suits have been passed.

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -16,7 +16,7 @@ export default function Main() {
       quote: list[idx].quote,
       author: list[idx].author
     });
-}
+  }
   
   React.useEffect(() => {
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
@@ -26,13 +26,15 @@ export default function Main() {
       });
   }, []);
 
-  console.log(list);
+  //console.log(list);
   
   return(
       <div id="quote-box">
             <p id="text">{Quotes.quote}</p>
-            <p>{Quotes.author}</p>
-            <button onClick={handle}>New Quote</button>
+            <p id="author">{Quotes.author}</p>
+            <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank">Tweet</a>
+            <button onClick={handle} id="new-quote">New Quote</button>
+           
       </div>
   );
 }
